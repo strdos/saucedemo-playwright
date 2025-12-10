e3b6803cb949d5f9dca5bb5e4e2629a0b2fd18b2
strdos <strdos@gmail.com>
2025-12-10 15:39:41 +0100

    feat(test-data): add users and login error message constants

---
+++ test-data/login-errors.ts
@@ -0,0 +1,6 @@
+export const loginErrors = {
+  usernameRequired: 'Epic sadface: Username is required',
+  passwordRequired: 'Epic sadface: Password is required',
+  invalidCredentials: 'Epic sadface: Username and password do not match any user in this service',
+  lockedOutUser: 'Epic sadface: Sorry, this user has been locked out.',
+};
---
+++ test-data/users.ts
@@ -0,0 +1,10 @@
+export const PASSWORD = 'secret_sauce';
+
+export const users = {
+  standard: 'standard_user',
+  lockedOut: 'locked_out_user',
+  problem: 'problem_user',
+  performance: 'performance_glitch_user',
+  error: 'error_user',
+  visual: 'visual_user',
+};
