9c23a2416c11fc73326af43b393d05d30cdd0ad2
strdos <strdos@gmail.com>
2025-12-14 21:08:07 +0100

    feat(cart-page): add initial CartPage page object

---
+++ pages/cartPage.ts
@@ -0,0 +1,32 @@
+import { Page } from '@playwright/test';
+
+export class CartPage {
+  static readonly url = '/cart.html';
+
+  constructor(private readonly page: Page) {}
+
+  readonly title = this.page.getByTestId('title'); // "Your Cart"
+
+  readonly itemCount = this.page.getByTestId('item-quantity');
+
+  readonly cartItems = this.page.getByTestId('inventory-item');
+  readonly itemNames = this.page.getByTestId('inventory-item-name');
+  readonly itemPrices = this.page.getByTestId('inventory_item_price');
+
+  readonly continueShoppingButton = this.page.getByTestId('continue-shopping');
+  readonly checkoutButton = this.page.getByTestId('checkout');
+
+  async goto() {
+    await this.page.goto(CartPage.url);
+  }
+
+  async getItemNames(): Promise<string[]> {
+    const texts = await this.itemNames.allTextContents();
+    return texts.map((t) => t.trim());
+  }
+
+  async getItemPrices(): Promise<number[]> {
+    const texts = await this.itemPrices.allTextContents();
+    return texts.map((t) => Number(t.replace(/[^0-9.-]/g, '')));
+  }
+}
