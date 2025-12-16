b649b18c79fc1d84e3f57279d39c62e2320c357b
strdos <strdos@gmail.com>
2025-12-16 19:56:17 +0100

    feat(checkout): add CheckoutStepOnePage page object

---
+++ pages/checkoutStepOne.ts
@@ -0,0 +1,37 @@
+import { Page } from '@playwright/test';
+
+export class CheckoutStepOnePage {
+  static readonly url = '/checkout-step-one.html';
+
+  constructor(private readonly page: Page) {}
+  readonly title = this.page.getByTestId('title'); // "Checkout: Your Information"
+  readonly firstNameInput = this.page.getByTestId('firstName');
+  readonly lastNameInput = this.page.getByTestId('lastName');
+  readonly postalCodeInput = this.page.getByTestId('postalCode');
+  readonly continueButton = this.page.getByTestId('continue');
+  readonly cancelButton = this.page.getByTestId('cancel');
+  readonly error = this.page.getByTestId('error');
+
+  async goto() {
+    await this.page.goto(CheckoutStepOnePage.url);
+  }
+
+  async fillCheckoutInformation(firstName: string, lastName: string, postalCode: string) {
+    await this.firstNameInput.fill(firstName);
+    await this.lastNameInput.fill(lastName);
+    await this.postalCodeInput.fill(postalCode);
+  }
+
+  async continueCheckout() {
+    await this.continueButton.click();
+  }
+
+  async cancel() {
+    await this.cancelButton.click();
+  }
+
+  async completeCheckoutInformation(firstName: string, lastName: string, postalCode: string) {
+    await this.fillCheckoutInformation(firstName, lastName, postalCode);
+    await this.continueCheckout();
+  }
+}
---
+++ test-data/checkout-errors.ts
@@ -0,0 +1,5 @@
+export const checkoutErrors = {
+  firstNameRequired: 'Error: First Name is required',
+  lastNameRequired: 'Error: Last Name is required',
+  postalCodeRequired: 'Error: Postal Code is required',
+};
